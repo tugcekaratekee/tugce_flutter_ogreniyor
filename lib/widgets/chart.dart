bf8acf3d3ebef1663b4aa003563331501265a977
tugcekaratekee <tugcekaratekee@gmail.com>
2023-02-10 20:59:08 +0300

    chart_bar added

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import './chart_bar.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -31,16 +32,34 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item?['amount'] as double);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print(groupedTransactionValues);
     return Card(
       elevation: 6,
-      margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return Text('${data?['day']}: ${data?['amount']}');
-        }).toList(),
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: Chartbar(
+                  data?['day'] as String,
+                  data?['amount'] as double,
+                  totalSpending == 0.0
+                      ? 0.0
+                      : (data?['amount'] as double) / totalSpending),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
