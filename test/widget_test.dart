fdf9493537a70874dc564be847b3af4b5bdffc57
tugcekaratekee <tugcekaratekee@gmail.com>
2023-02-12 22:36:23 +0300

    Flexible&Expanded and adding a ListTile Widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tugce_flutter_ogreniyor/main.dart';
+import 'package:tugce_flutter_ogreniyor/main3.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
